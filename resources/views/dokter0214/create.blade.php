cab8bd6fb56b585a8496a0e85f13024d77a87ef2
RIzqiFauzaandyPrayogi <rizqifauzaandyprayogi@gmail.com>
2021-06-16 01:19:16 +0700

    Praktikum 4

--- resources/views/dokter0214/create.blade.php
+++ resources/views/dokter0214/create.blade.php
@@ -8,7 +8,7 @@
     <form action="{{ route('dokter0214.store') }}" method="POST" enctype="multiport/from-data">
         @csrf
         <label for="file">Pilih Excel</label><br></br>
-        <input type="file" id="file" name="file" accept=".csv, ">
+        <input type="file" id="file" name="file" accept=".xlsx, ">
         <button type="submit" value="submit">import</button>
     </form>
 </div>
